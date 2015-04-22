a60fe1092df43eba1efe3b0285ccf84879348c95
Jacob D. Parr <me@jacobparr.com>
2015-04-22 16:43:13 -0700

    Updated server-start.bat to use javaw

--- to-proto.bat
+++ to-proto.bat
@@ -1,5 +1,4 @@
 @echo off
-copy C:\dvlp\3rd-party\tioga-solutions\Jobs\tioga-jobs-agent\build\libs\tioga-jobs-agent-1.0.4.jar     \\parwinproto\d$\proto\tioga-jobs-agent\lib
-copy C:\dvlp\3rd-party\tioga-solutions\Jobs\tioga-jobs-client\build\libs\tioga-jobs-client-1.0.1.jar   \\parwinproto\d$\proto\tioga-jobs-agent\lib
-copy C:\dvlp\3rd-party\tioga-solutions\Jobs\tioga-jobs-jackson\build\libs\tioga-jobs-jackson-4.0.4.jar \\parwinproto\d$\proto\tioga-jobs-agent\lib
-copy C:\dvlp\3rd-party\tioga-solutions\Jobs\tioga-jobs-pub\build\libs\tioga-jobs-pub-1.0.0.jar         \\parwinproto\d$\proto\tioga-jobs-agent\lib
+del \\parwinproto\d$\proto\tioga-jobs-agent\lib\*.jar
+copy C:\dvlp\3rd-party\tioga-solutions\Jobs\tioga-jobs-agent\build\install\tioga-jobs-agent\lib\*.jar \\parwinproto\d$\proto\tioga-jobs-agent\lib
+copy C:\dvlp\3rd-party\tioga-solutions\Jobs\tioga-jobs-agent\build\install\tioga-jobs-agent\server-start.bat \\parwinproto\d$\proto\tioga-jobs-agent\
