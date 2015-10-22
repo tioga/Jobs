be310dc0589874935f0fc6e77fec25b8a1738b2c
= <=>
2015-10-21 17:56:00 -0700

    Fixed broken unit tests

--- tioga-jobs-kernel/src/test/java/org/tiogasolutions/jobs/pub/actions/OsActionTest.java
+++ tioga-jobs-kernel/src/test/java/org/tiogasolutions/jobs/pub/actions/OsActionTest.java
@@ -2,11 +2,13 @@ package org.tiogasolutions.jobs.pub.actions;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.tiogasolutions.dev.common.OsUtils;
 import org.tiogasolutions.dev.common.json.JsonTranslator;
 import org.tiogasolutions.dev.jackson.TiogaJacksonTranslator;
 import org.tiogasolutions.jobs.jackson.JobsObjectMapper;
 import org.tiogasolutions.jobs.pub.ActionType;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
@@ -18,9 +20,13 @@ public class OsActionTest {
     JobsObjectMapper objectMapper = new JobsObjectMapper();
     JsonTranslator translator = new TiogaJacksonTranslator(objectMapper);
 
-    OsAction oldAction = OsAction.create("c:\\Windows\\system32\\cmd.exe /c dir", Paths.get("c:\\dvlp"), "my-lock", 10, TimeUnit.SECONDS);
+    Path workingDir = OsUtils.isWindows() ? Paths.get("c:\\dvlp") : Paths.get("/dvlp");
+
+    OsAction oldAction = OsAction.create("c:\\Windows\\system32\\cmd.exe /c dir", workingDir, "my-lock", 10, TimeUnit.SECONDS);
     String json = translator.toJson(oldAction);
-    Assert.assertEquals(json, EXPECTED_JSON);
+
+    if (OsUtils.isWindows()) Assert.assertEquals(json, EXPECTED_JSON_WINDOWS);
+    else Assert.assertEquals(json, EXPECTED_JSON_LINUX);
 
     OsAction newAction = translator.fromJson(OsAction.class, json);
 
@@ -32,14 +38,23 @@ public class OsActionTest {
     Assert.assertEquals(newAction.getTimeoutUnit(), TimeUnit.SECONDS);
   }
 
-  @SuppressWarnings("FieldCanBeLocal")
-  private final String EXPECTED_JSON =
+  private static final String EXPECTED_JSON_WINDOWS =
     "{\n" +
-    "  \"actionType\" : \"OS_COMMAND\",\n" +
-    "  \"command\" : \"c:\\\\Windows\\\\system32\\\\cmd.exe /c dir\",\n" +
-    "  \"workingDirectory\" : \"c:\\\\dvlp\",\n" +
-    "  \"lock\" : \"my-lock\",\n" +
-    "  \"timeout\" : 10,\n" +
-    "  \"timeoutUnit\" : \"SECONDS\"\n" +
-    "}";
+      "  \"actionType\" : \"OS_COMMAND\",\n" +
+      "  \"command\" : \"c:\\\\Windows\\\\system32\\\\cmd.exe /c dir\",\n" +
+      "  \"workingDirectory\" : \"c:\\\\dvlp\",\n" +
+      "  \"lock\" : \"my-lock\",\n" +
+      "  \"timeout\" : 10,\n" +
+      "  \"timeoutUnit\" : \"SECONDS\"\n" +
+      "}";
+
+  private static final String EXPECTED_JSON_LINUX =
+    "{\n" +
+      "  \"actionType\" : \"OS_COMMAND\",\n" +
+      "  \"command\" : \"c:\\\\Windows\\\\system32\\\\cmd.exe /c dir\",\n" +
+      "  \"workingDirectory\" : \"/dvlp\",\n" +
+      "  \"lock\" : \"my-lock\",\n" +
+      "  \"timeout\" : 10,\n" +
+      "  \"timeoutUnit\" : \"SECONDS\"\n" +
+      "}";
 }
